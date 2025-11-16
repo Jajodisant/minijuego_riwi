9be14cec9c5a19f5ba796ef38b64ea3557a69f75
Jajodisant <pabonleonardo419@gmail.com>
2025-11-16 11:51:13 -0500

    main

--- main.py
+++ main.py
@@ -4,22 +4,23 @@ def main():
     inventory_1 = Inventory()
 
     print("------------------INVENTORY-----------------")
-    inventory_1.show_inventary()
-    inventory_1.add_product_to_inventory(Type="Pig", quantity=2, price= 120)
-    inventory_1.add_product_to_inventory(Type="Cow", quantity=1, price= 200)
-    inventory_1.add_product_to_inventory(Type="Goat", quantity=5, price= 170)
+    inventory_1.show_inventory()
+
+    inventory_1.add_product(type="Pig", quantity=2, price=120)
+    inventory_1.add_product(type="Cow", quantity=1, price=200)
+    inventory_1.add_product(type="Goat", quantity=5, price=170)
 
     print("------------------INVENTORY-----------------")
-    inventory_1.show_inventary()
+    inventory_1.show_inventory()
 
     print("------------------GET PRODUCT-----------------")
     print(inventory_1.get_product(type="Cow"))
 
     inventory_1.delete_product(type="Pig", delete_all=True)
+
     print("------------------INVENTORY-----------------")
-    inventory_1.show_inventary()
+    inventory_1.show_inventory()
 
 
 if __name__ == "__main__":
     main()
-
